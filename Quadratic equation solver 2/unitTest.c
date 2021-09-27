598a0eea8070d21c5e9c8cdd61624b15f02ebdaa
KushnerovMS <89263262+KushnerovMS@users.noreply.github.com>
2021-09-27 22:38:02 +0300

    Add files via upload

--- Quadratic equation solver 2/unitTest.c
+++ Quadratic equation solver 2/unitTest.c
@@ -2,7 +2,7 @@
 #include <math.h>
 #include <float.h>
 
-const double EPSILON = 1e-100;
+const double EPSILON = 1e-50;
 
 void testFunction(double a, double b, double c, int curRCount, double curX1, double curX2, int (*functionToTest)(double a, double b, double c, double *x1, double *x2))
 {
@@ -12,7 +12,9 @@ void testFunction(double a, double b, double c, int curRCount, double curX1, dou
 
     rCount = (*functionToTest)(a, b, c, &x1, &x2);
 
-    if(rCount != curRCount || fabs((curX1 - x1) / curX1) > EPSILON || fabs((curX2 - x2) / curX2) > EPSILON)
+    if(rCount != curRCount ||
+    (fabs((curX1 - x1) / curX1) > EPSILON && fabs((curX1 - x2) / curX1) > EPSILON) ||
+    (fabs((curX2 - x2) / curX2) > EPSILON && fabs((curX2 - x1) / curX2) > EPSILON) )
     {
         printf("a = %e, b = %e, c = %e\n", a, b, c);
         printf("Current Root count = %d, x1 = %e, x2 = %e\n", curRCount, curX1, curX2);
@@ -22,15 +24,16 @@ void testFunction(double a, double b, double c, int curRCount, double curX1, dou
 
 void unitTest(int (*functionToTest)(double a, double b, double c, double *x1, double *x2))
 {
-    testFunction(/*a*/1,/*b*/2,/*c*/1,/*n*/1,/*x1*/-1,/*x2*/0, functionToTest);
-    testFunction(/*a*/1*DBL_MIN,/*b*/2*DBL_MIN,/*c*/1*DBL_MIN,/*n*/1,/*x1*/-1,/*x2*/0, functionToTest);
-    testFunction(/*a*/0.5*DBL_MAX,/*b*/1*DBL_MAX,/*c*/0.5*DBL_MAX,/*n*/1,/*x1*/-1,/*x2*/0, functionToTest);
-    testFunction(/*a*/1*sqrt(DBL_MIN),/*b*/2*sqrt(DBL_MIN),/*c*/1*sqrt(DBL_MIN),/*n*/1,/*x1*/-1,/*x2*/0, functionToTest);
-    testFunction(/*a*/1*sqrt(DBL_MAX),/*b*/2*sqrt(DBL_MAX),/*c*/1*sqrt(DBL_MAX),/*n*/1,/*x1*/-1,/*x2*/0, functionToTest);
-    testFunction(/*a*/0,/*b*/0,/*c*/0,/*n*/-1,/*x1*/0,/*x2*/0, functionToTest);
-    testFunction(/*a*/0,/*b*/0,/*c*/1,/*n*/0,/*x1*/0,/*x2*/0, functionToTest);
-    testFunction(/*a*/0,/*b*/2,/*c*/1,/*n*/1,/*x1*/-0.5,/*x2*/0, functionToTest);
-    testFunction(/*a*/1,/*b*/0,/*c*/1,/*n*/0,/*x1*/0,/*x2*/0, functionToTest);
-    testFunction(/*a*/1,/*b*/0,/*c*/0,/*n*/1,/*x1*/0,/*x2*/0, functionToTest);
-    testFunction(/*a*/1,/*b*/3,/*c*/2,/*n*/2,/*x1*/-2,/*x2*/-1, functionToTest);
+    //           a      b       c       n      x1      x2
+    testFunction(1,     2,      1,      1,     -1,      0, functionToTest);
+    testFunction(0,     0,      0,     -1,      0,      0, functionToTest);
+    testFunction(0,     0,      1,      0,      0,      0, functionToTest);
+    testFunction(0,     2,      1,      1,   -0.5,      0, functionToTest);
+    testFunction(1,     0,      1,      0,      0,      0, functionToTest);
+    testFunction(1,     0,      0,      1,      0,      0, functionToTest);
+    testFunction(1,     3,      2,      2,     -2,     -1, functionToTest);
+    testFunction(1*DBL_MIN,2*DBL_MIN,1*DBL_MIN,1,-1,0, functionToTest);
+    testFunction(0.5*DBL_MAX,1*DBL_MAX,0.5*DBL_MAX,1,-1,0, functionToTest);
+    testFunction(1*sqrt(DBL_MIN),2*sqrt(DBL_MIN),1*sqrt(DBL_MIN),1,-1,0, functionToTest);
+    testFunction(1*sqrt(DBL_MAX),2*sqrt(DBL_MAX),1*sqrt(DBL_MAX),1,-1,0, functionToTest);
 }
